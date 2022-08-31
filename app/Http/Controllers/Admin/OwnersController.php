1691fa6cfa4ebbe7260bf9860de7968d685d0a59
pyusuket <shimomura8939@gmail.com>
2022-08-31 17:01:02 +0900

    showファイル・createファイルの作成

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -21,21 +21,22 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $data_now = Carbon::now();
-        $data_parse = Carbon::parse(now());
-        echo $data_now;
-        echo $data_parse;
+        // $data_now = Carbon::now();
+        // $data_parse = Carbon::parse(now());
+        // echo $data_now;
+        // echo $data_parse;
 
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
-        // $q_first = DB::table('owners')->select('name')->first();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
         //     'name' => 'テスト'
         // // ]);
         // dd($e_all, $q_get, $q_first, $c_test);
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+        $owners = Owner::select('name', 'email', 'created_at')->get();
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -45,7 +46,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
