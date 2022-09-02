d8ce23ef60083fbf867015630e44c5356fda1f27
pyusuket <shimomura8939@gmail.com>
2022-09-02 15:20:52 +0900

    コントローラ（destory)

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Owner;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Database\Console\Migrations\StatusCommand;
 use Illuminate\Support\Facades\Hash;
 class OwnersController extends Controller
 {
@@ -72,7 +73,7 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー登録を実施しました。');
+        ->with(['message', 'オーナー登録を実施しました。','status' => 'info']);
     }
 
     /**
@@ -116,7 +117,7 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message' , 'オーナー情報を更新しました。');
+        ->with(['message' , 'オーナー情報を更新しました。','status' => 'info']);
     }
 
     /**
@@ -127,6 +128,19 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message' => 'オーナー情報を削除しました。','status' => 'alert']);
     }
+
+    public function expiredOwnerIndex(){
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners',compact('expiredOwners'));
+       }
+       public function expiredOwnerDestroy($id){
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index');
+       }
 }
